20054e9b81039469d8bfd0e876a07962497cb78f
Sandra <sandra.gufler@gmx.ch>
2022-12-05 09:39:11 +0100

    initial commit to see if repo works

---
+++ mergeTwoSortedLists.js
@@ -0,0 +1,10 @@
+/*
+Here you can find the problem: https://leetcode.com/problems/merge-two-sorted-lists/
+Below you find a fuction that prints the solution to the console.
+*/
+
+const mergeTwoSortedLists = (l1, l2) => {
+  let result = [];
+
+  console.log(result);
+};
